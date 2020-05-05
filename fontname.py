17a84cc9e7897d25f43bad89154805052b8b1a3d
John Preston <johnprestonmail@gmail.com>
2020-05-05 13:11:10 +0400

    Change more fields + allow marking font as bold.

--- fontname.py
+++ fontname.py
@@ -21,21 +21,19 @@ from __future__ import unicode_literals
 
 import sys
 import os
-
 from fontTools import ttLib
 from fontTools.misc.py23 import tounicode, unicode
-
 
 def main(argv):
     # command argument tests
     print(" ")
-    if len(argv) < 2:
+    if len(argv) < 3:
         sys.stderr.write(
             "[fontname.py] ERROR: you did not include enough arguments to the script."
             + os.linesep
         )
         sys.stderr.write(
-            "Usage: python fontname.py [FONT FAMILY NAME] [FONT PATH 1] <FONT PATH ...>"
+            "Usage: python fontname.py [FONT FAMILY NAME] [FONT PATH FROM] [FONT PATH TO]"
             + os.linesep
         )
         sys.exit(1)
@@ -51,26 +49,34 @@ def main(argv):
         )
         sys.exit(1)
 
-    font_path_list = argv[
-        1:
-    ]  # all remaining arguments on command line are file paths to fonts
+    font_path_src = argv[1]
+    font_path_dst = argv[2]
+    mark_as_bold = (len(argv) > 3) and (argv[3] == 'mark_as_bold')
 
-    # iterate through all paths provided on command line and rename to `font_name` defined by user
-    for font_path in font_path_list:
-        # test for existence of font file on requested file path
-        if not file_exists(font_path):
-            sys.stderr.write(
-                "[fontname.py] ERROR: the path '"
-                + font_path
-                + "' does not appear to be a valid file path."
-                + os.linesep
-            )
-            sys.exit(1)
+    # test for existence of font file on requested file path
+    if not file_exists(font_path_src):
+        sys.stderr.write(
+            "[fontname.py] ERROR: the path '"
+            + font_path_src
+            + "' does not appear to be a valid file path."
+            + os.linesep
+        )
+        sys.exit(1)
 
-        tt = ttLib.TTFont(font_path)
-        namerecord_list = tt["name"].names
-        variant = ""
+    tt = ttLib.TTFont(font_path_src)
+    namerecord_list = tt["name"].names
+    variant = ''
 
+    if font_path_dst == 'print':
+        print('HEAD macStyle: ' + str(tt['head'].macStyle))
+        print('OS/2 fsSelection: ' + str(tt['OS/2'].fsSelection))
+        for record in namerecord_list:
+            print(str(record.nameID) + ' ' + record.toUnicode())
+        sys.exit(0)
+    elif mark_as_bold:
+        tt['head'].macStyle |= 1
+        tt['OS/2'].fsSelection = 32 | (tt['OS/2'].fsSelection & 1)
+    else:
         # determine font variant for this file path from name record nameID 2
         for record in namerecord_list:
             if record.nameID == 2:
@@ -83,7 +89,7 @@ def main(argv):
         if len(variant) == 0:
             sys.stderr.write(
                 "[fontname.py] Unable to detect the font variant from the OpenType name table in '"
-                + font_path
+                + font_path_src
                 + "'."
                 + os.linesep
             )
@@ -104,30 +110,38 @@ def main(argv):
             for record in namerecord_list:
                 if record.nameID == 1:
                     record.string = nameID1_string
+                elif record.nameID == 2:
+                    record.string = variant
+                elif record.nameID == 3:
+                    parts = record.toUnicode().split(';')
+                    if len(parts) == 3:
+                        record.string = parts[0] + ';' + parts[1] + ';' + nameID6_string
                 elif record.nameID == 4:
                     record.string = nameID4_string
                 elif record.nameID == 6:
                     record.string = nameID6_string
+                elif record.nameID == 16:
+                    record.string = nameID1_string
 
-        # write changes to the font file
-        try:
-            tt.save(font_path)
-            print(
-                "[OK] Updated '"
-                + font_path
-                + "' with the name '"
-                + nameID4_string
-                + "'"
-            )
-        except Exception as e:
-            sys.stderr.write(
-                "[fontname.py] ERROR: unable to write new name to OpenType tables for '"
-                + font_path
-                + "'."
-                + os.linesep
-            )
-            sys.stderr.write(unicode(e))
-            sys.exit(1)
+        for record in namerecord_list:
+            print(str(record.nameID) + ' ' + record.toUnicode())
+
+    # write changes to the font file
+    try:
+        tt.save(font_path_dst)
+        print(
+            "[OK] Updated '"
+            + font_path_dst
+        )
+    except Exception as e:
+        sys.stderr.write(
+            "[fontname.py] ERROR: unable to write new name to OpenType tables for '"
+            + font_path
+            + "'."
+            + os.linesep
+        )
+        sys.stderr.write(unicode(e))
+        sys.exit(1)
 
 
 # Utilities
